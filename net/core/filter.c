e1ee4e7e57ebec333d94d561dad55fc961deea44
Daniel Borkmann <daniel@iogearbox.net>
2016-12-04 23:19:39 +0100

    bpf: remove type arg from __is_valid_{,xdp_}access
    Commit d691f9e8d440 ("bpf: allow programs to write to certain skb
    fields") pushed access type check outside of __is_valid_access()
    to have different restrictions for socket filters and tc programs.
    type is thus not used anymore within __is_valid_access() and should
    be removed as a function argument. Same for __is_valid_xdp_access()
    introduced by 6a773a15a1e8 ("bpf: add XDP prog type for early driver
    filter").
    
    Change-Id: Ida7c9137fa8bf9fc10ab5d7ca1bbd9a05acaa9aa
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Acked-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 1afaf661b20a075a22eef3332db187001a334b81)

--- net/core/filter.c
+++ net/core/filter.c
@@ -3589,8 +3589,7 @@ static bool tc_cls_act_is_valid_access(int off, int size,
 	return bpf_skb_is_valid_access(off, size, type, info);
 }
 
-static bool __is_valid_xdp_access(int off, int size,
-				  enum bpf_access_type type)
+static bool __is_valid_xdp_access(int off, int size)
 {
 	if (off < 0 || off >= sizeof(struct xdp_md))
 		return false;
@@ -3618,7 +3617,7 @@ static bool xdp_is_valid_access(int off, int size,
 		break;
 	}
 
-	return __is_valid_xdp_access(off, size, type);
+	return __is_valid_xdp_access(off, size);
 }
 
 void bpf_warn_invalid_xdp_action(u32 act)
